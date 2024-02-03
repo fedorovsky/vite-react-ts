ee27e0d39eb22f83fd11fa1554ff98f335725bc2
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2024-02-03 10:43:59 +0100

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider, ReactReduxContext } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import store from './store';
+import App from './App.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store} context={ReactReduxContext}>
+      <ThemeProvider theme={{}}>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
+);
