4d7fc029ca29e8dd86553b86ee12e941555c44ed
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2024-05-13 22:20:30 +0200

    modal-data

--- src/app/upsell-offers/redux/asyncActions.ts
+++ src/app/upsell-offers/redux/asyncActions.ts
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { _SLICE_NAME_ } from './constants';
-import { SelectedPack, ModalData } from './types';
+import { BillingPack, ModalData } from './types';
 import { deferredObject } from './utils';
 
-export const openModalWithPromise = createAsyncThunk<SelectedPack, ModalData>(
+export const openModalWithPromise = createAsyncThunk<BillingPack, ModalData>(
   `${_SLICE_NAME_}/openModalWithPromise`,
   async () => {
     return new Promise((res, rej) => {
