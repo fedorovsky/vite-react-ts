776f6be4dd88a40e7fd1dc08349434ad804b72f5
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2024-08-15 11:14:47 +0200

    Added Waiting List table

--- src/app/global-modals/components/debugger/WaitingList.tsx
+++ src/app/global-modals/components/debugger/WaitingList.tsx
@@ -8,40 +8,36 @@ import { useAppSelector } from '@/core/hooks/useAppSelector.ts';
 export const WaitingList = () => {
   const waitingList = useAppSelector(globalModalsModule.selectors.waitingList);
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th className="py-1 px-2 border-b border-gray-200 text-left">
-              Modal Name
-            </th>
-            <th className="py-1 px-2 border-b border-gray-200 text-left">
-              Priority Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {waitingList.map((item) => (
-            <ModalPriorityDisplay key={item} modalName={item} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <thead className="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th className="px-3 py-2">Waiting List</th>
+          <th className="px-3 py-2">Modal Name</th>
+          <th className="px-3 py-2">Priority Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {waitingList.map((item, index) => (
+          <ModalPriorityDisplay index={index + 1} key={item} modalName={item} />
+        ))}
+      </tbody>
+    </table>
   );
 };
 
 const ModalPriorityDisplay = ({
+  index,
   modalName,
 }: {
+  index: number;
   modalName: GLOBAL_MODAL_NAME;
 }) => {
   const { isPriority } = usePriorityModal(modalName);
   return (
     <tr className={`${isPriority ? 'bg-green-500' : 'bg-red-500'}`}>
-      <td className="py-1 px-2 border-b border-gray-200 text-left">
-        {modalName}
-      </td>
-      <td className="py-1 px-2 border-b border-gray-200 text-left">
+      <td className="px-3 border-b dark:border-gray-700">{index}</td>
+      <td className="px-3 border-b dark:border-gray-700">{modalName}</td>
+      <td className="px-3 border-b dark:border-gray-700">
         {isPriority ? 'Priority' : 'Not Priority'}
       </td>
     </tr>
