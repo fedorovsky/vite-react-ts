cf3e7b88f35d149eb99ae5b962a83dfd7859a193
a.fedorovskyi <anton.fedorovsky@gmail.com>
2025-09-20 20:43:16 +0200

    dnd-metadata-picker

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import Default from '@/variants/default.tsx';
 import Move from '@/variants/move.tsx';
 import DnD from '@/variants/dnd.tsx';
 import DndMetadata from '@/variants/dnd-metadata.tsx';
+import DndMetadataPicker from '@/variants/dnd-metadata-picker.tsx';
 
 export default function App() {
   return (
     <div>
       {/*<Default />*/}
       {/*<Move />*/}
-      <DnD />
+      {/*<DnD />*/}
       {/*<DndMetadata />*/}
+      <DndMetadataPicker />
     </div>
   );
 }
