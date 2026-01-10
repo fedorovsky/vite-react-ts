669f3940dd06eb4821958705a654ee3e018bfbbb
a.fedorovskyi <anton.fedorovsky@gmail.com>
2026-01-10 18:46:47 +0100

    playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,3 @@
-import { useAppDispatch } from '@/core/hooks/useAppDispatch';
-import { userApi } from '@/app/user/api';
-import { UserList } from '@/app/user';
-import UserListRefetchOnMount from '@/app/user/components/user-list-refetch-on-mount';
-import { ToggleContent } from '@/shared/component/toggle-content';
-
 export default function App() {
-  const dispatch = useAppDispatch();
-
-  const handleClickInvalidate = () => {
-    dispatch(userApi.util.invalidateTags(['User']));
-  };
-
-  return (
-    <div>
-      <button onClick={handleClickInvalidate}>Invalidate Tag</button>
-      <UserList />
-      <ToggleContent text="UserList">
-        <UserList />
-      </ToggleContent>
-      <ToggleContent text="UserList">
-        <UserList />
-      </ToggleContent>
-      <ToggleContent text="UserList - refetchOnMountOrArgChange: 10">
-        <UserListRefetchOnMount />
-      </ToggleContent>
-    </div>
-  );
+  return <div>hello</div>;
 }
