2828096cb0de0090225109fc1b0bf6736e16a90d
a.fedorovskyi <anton.fedorovsky@gmail.com>
2026-01-10 18:50:55 +0100

    added react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import { ThemeProvider } from 'styled-components';
 import { store } from '@/core/store/store.ts';
 import App from './App.tsx';
 
+if (process.env.NODE_ENV === 'development') {
+  import('react-scan').then(({ scan }) => {
+    scan();
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store} context={ReactReduxContext}>
