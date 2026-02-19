afe84299820efa5820b621109feab61c37857a0d
a.fedorovskyi <anton.fedorovsky@gmail.com>
2026-02-19 14:28:56 +0100

    added user slice

--- src/features/user/api/index.ts
+++ src/features/user/api/index.ts
@@ -1,8 +1,1 @@
-import { api } from '@/core/api';
-import { User } from '../redux/types.ts';
-
-export const getUserListData = async () => {
-  const userList = await api<User[]>('http://localhost:8000/users');
-
-  return userList;
-};
+export * from './get-user-list-data.ts';
